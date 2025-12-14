5dc0db2d988c4a948d03fe8f2386befe1aa5ea29
Saendhor <comix143.mariorossi@gmail.com>
2025-12-14 12:23:31 +0100

    fix: SimpleApp's servlet not working

---
+++ Java/esempi/SimpleApp/src-SimpleApp/SimpleServlet.java
@@ -0,0 +1,16 @@
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.*;
+
+public class SimpleServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //select the content type to respond
+        response.setContentType("text/html");
+
+        //create and send a text to client
+        PrintWriter out = response.getWriter();
+        
+        out.println("Output della MySimpleServlet");
+        out.close();
+    }
+}
