ab04e4c0ce85f2bef3a91b3cf117f743bebf4107
Saendhor <comix143.mariorossi@gmail.com>
2025-12-17 12:20:25 +0100

    feat: implemented Read and Create successfully

--- Java/esercizi/MovieCatalogue/src_code/MovieCatalogue.java
+++ Java/esercizi/MovieCatalogue/src_code/MovieCatalogue.java
@@ -74,9 +74,14 @@ public class MovieCatalogue extends HttpServlet {
                     //add Update and Delete form
 
                     out.println("<br>");
-
-                    //add Create form
                 }
+
+                //add Create form
+                out.println("""
+                    <form action = 'MovieCatalogue' method = 'post'>
+                    <input type = 'submit' name = 'action' value = 'create'>
+                    </form>
+                """);
 
             } catch (SQLException e) {
                 e.printStackTrace();
@@ -86,4 +91,72 @@ public class MovieCatalogue extends HttpServlet {
             out.println("<br>");
         }
     }
+
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+        out = response.getWriter();
+
+        String action = request.getParameter("action");
+        String query = " ";
+
+        //Check if the action is not null
+        if (action != null) {
+            switch(action) {
+                case "create":
+                    //| id | title | director | year | duration_minutes | genre |
+                    out.println("""
+                        <form action = 'MovieCatalogue' method = 'post'>
+                        <input type = 'text' name = 'title' placeholder = 'title' required> <br>
+                        <input type = 'text' name = 'director' placeholder = 'director' required> <br>
+                        <input type = 'text' name = 'year' placeholder = 'year' required> <br>
+                        <input type = 'text' name = 'duration_minutes' placeholder = 'duration(min)' required> <br>
+                        <input type = 'text' name = 'genre' placeholder = 'genre' required> <br>
+                        <input type = 'submit' name = 'action' value = 'confirmCreate'>
+                        </form>
+                    """);
+                    break;
+                
+                case "confirmCreate":
+                    String title = request.getParameter("title");
+                    String director = request.getParameter("director");
+                    int year = Integer.parseInt(request.getParameter("year"));
+                    int duration = Integer.parseInt(request.getParameter("duration_minutes"));
+                    String genre = request.getParameter("genre");
+
+                    query = "INSERT INTO movies (title, director, year, duration_minutes, genre)"
+                    + "VALUES ('" + title + "','"+ director +"','"+ year +"','"+ duration +"','"+ genre +"');";
+
+                    try {
+                        Statement stmt = connection.createStatement();
+                        ResultSet result = stmt.executeQuery(query);
+                        
+                        out.println("New item added successfully!");
+                        out.println("<br>");
+                        out.println("<a href=/MovieCatalogue>Torna alla homepage</a>");
+
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+
+                    break;
+                
+                case "update":
+                    break;
+
+                case "delete":
+                    break;
+                
+                default:
+                    out.println("Action not recognized");
+                    out.println("<br>");
+            }
+
+        } else {
+            out.println("Submitted action is null");
+            out.println("<br>");
+        }
+    }
+
 }
